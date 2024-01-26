0158fabfcd7a386d7107827d939accf5ae763db8
EkaterinaTokar <ekaterinatokar19@gmail.com>
2024-01-26 15:46:42 +0500

    feat: implemented hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from "node:crypto";
+import fs from "node:fs";
+import * as url from "url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(
+    url.fileURLToPath(
+      new URL("files/fileToCalculateHashFor.txt", import.meta.url)
+    )
+  );
+  readStream.on("data", (chunk) => {
+    const hash = crypto.createHash("sha256").update(chunk).digest("hex");
+  });
+  readStream.on("error", (error) => {
+    console.log(error);
+  });
+  readStream.on("end", () => {
+    console.log(`hash: ${hash}`);
+  });
 };
 
 await calculateHash();
